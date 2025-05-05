aee0b9e93a3903640a58998c9661e980de353520
JXtaiver <coolhawk.joeyxt.com@gmail.com>
2025-05-05 13:48:32 -0400

    Update Graph.java

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -1,36 +1,64 @@
-import java.util.*;
+
 
 class Graph {
-    private int[][] adjMatrix;
-    private String[] vertexData;
+    private int[][] adjMatrix; // Adjacency matrix to represent edges (for unweighted graph)
+    private String[] vertexData; // Data associated with each vertex
     private int size;
+    private boolean isWeighted; // Flag to check if the graph is weighted
+    private int[][] weightMatrix; // Matrix to store edge weights (for weighted graph)
 
+    // Constructor for unweighted graph
     public Graph(int size) {
         this.size = size;
         this.adjMatrix = new int[size][size];
         this.vertexData = new String[size];
+        this.isWeighted = false;
     }
 
-    public void addEdge(int u, int v) {
-        if (u >= 0 && u < size && v >= 0 && v < size) {
+    // Constructor for weighted graph
+    public Graph(int size, boolean isWeighted) {
+        this(size); // Calling the basic constructor
+        this.isWeighted = isWeighted;
+        if (isWeighted) {
+            this.weightMatrix = new int[size][size];
+        }
+    }
+
+    // Add edge method (for unweighted and weighted graphs)
+    public void addEdge(int u, int v, int weight) {
+        if (isWeighted) {
+            weightMatrix[u][v] = weight;
+            weightMatrix[v][u] = weight;
+        } else {
             adjMatrix[u][v] = 1;
             adjMatrix[v][u] = 1; // undirected
         }
     }
 
+    // Add vertex data
     public void addVertexData(int vertex, String data) {
         if (vertex >= 0 && vertex < size) {
             vertexData[vertex] = data;
         }
     }
 
+    // Print graph (adjacency matrix and vertex data)
     public void printGraph() {
         System.out.println("Adjacency Matrix:");
-        for (int[] row : adjMatrix) {
-            for (int cell : row) {
-                System.out.print(cell + " ");
+        if (isWeighted) {
+            for (int[] row : weightMatrix) {
+                for (int cell : row) {
+                    System.out.print(cell + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
+        } else {
+            for (int[] row : adjMatrix) {
+                for (int cell : row) {
+                    System.out.print(cell + " ");
+                }
+                System.out.println();
+            }
         }
         System.out.println("\nVertex Data:");
         for (int i = 0; i < size; i++) {
@@ -38,16 +66,18 @@ class Graph {
         }
     }
 
+    // DFS method
     private void dfsUtil(int v, boolean[] visited) {
         visited[v] = true;
         System.out.print(vertexData[v] + " ");
         for (int i = 0; i < size; i++) {
-            if (adjMatrix[v][i] == 1 && !visited[i]) {
+            if ((isWeighted ? weightMatrix[v][i] > 0 : adjMatrix[v][i] == 1) && !visited[i]) {
                 dfsUtil(i, visited);
             }
         }
     }
 
+    // DFS traversal starting from a specific vertex
     public void dfs(String startVertexData) {
         boolean[] visited = new boolean[size];
         int startVertex = -1;
@@ -66,61 +96,28 @@ class Graph {
         System.out.println();
     }
 
-    // 1. Incidence method
-    public boolean incidence(Set<String> setA, Set<String> setB) {
-        return setB.containsAll(setA);
-    }
-
-    // 2. Degree method
+    // Degree method (returns degree of each vertex)
     public int[] degree() {
         int[] degrees = new int[size];
         for (int i = 0; i < size; i++) {
             int count = 0;
             for (int j = 0; j < size; j++) {
-                count += adjMatrix[i][j];
+                if (isWeighted ? weightMatrix[i][j] > 0 : adjMatrix[i][j] == 1) {
+                    count++;
+                }
             }
             degrees[i] = count;
         }
         return degrees;
     }
 
-    // 3. Isomorphism method
-    public boolean isIsomorphic(Graph other) {
-        if (this.size != other.size) return false;
-        int[] thisDeg = this.degree();
-        int[] otherDeg = other.degree();
-        Arrays.sort(thisDeg);
-        Arrays.sort(otherDeg);
-        return Arrays.equals(thisDeg, otherDeg);
-    }
-
-    // 4. Power Set method
-    public List<Set<String>> powerSet(Set<String> originalSet) {
-        List<Set<String>> subsets = new ArrayList<>();
-        List<String> list = new ArrayList<>(originalSet);
-        int n = list.size();
-        for (int i = 0; i < (1 << n); i++) {
-            Set<String> subset = new HashSet<>();
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) != 0) {
-                    subset.add(list.get(j));
-                }
-            }
-            subsets.add(subset);
-        }
-        return subsets;
-    }
-    public String[] getVertexData() {
-        return vertexData;
-    }
-    
-    // 5. Euler characteristic method
+    // Euler characteristic for planar graphs
     public void euler() {
         int V = size;
         int E = 0;
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
-                if (adjMatrix[i][j] == 1) E++;
+                if ((isWeighted ? weightMatrix[i][j] > 0 : adjMatrix[i][j] == 1)) E++;
             }
         }
         int F = 1 + E - V; // Only for planar graphs
